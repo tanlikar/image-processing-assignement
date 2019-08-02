04fa77a41087e2b409961b187a9adeeb769224b8
Tan Li Kar <tanlikar@gmail.com>
2019-08-03 01:36:13 +0800

    add comment

--- random forest.py
+++ random forest.py
@@ -15,6 +15,9 @@ dataset = pd.read_excel('imageDatanewTEST.xlsx', index_col=None, header=0)
 X = dataset.iloc[19:,[0,2,3,4,8]].values #day 2 only
 y = dataset.iloc[:, 12].values
 
+
+#due to y are floating value, need to encode the y value
+#0 = control, 1 = 0.25, 2= 0.5, 3= 1.0, 4=2.0, 5=4.0, 6=8.0
 from sklearn import preprocessing
 from sklearn import utils
 lab_enc = preprocessing.LabelEncoder()
@@ -40,6 +43,7 @@ classifierDT = DecisionTreeClassifier(criterion = 'entropy', random_state = 1)
 
 from sklearn.metrics import confusion_matrix
 
+#Initialize K-Fold of 19-Fold
 from sklearn.model_selection import KFold 
 kf = KFold(n_splits=19, random_state=0, shuffle=True) 
 
@@ -65,15 +69,17 @@ for train_index, test_index in kf.split(X):
       print("Train:", train_index, "Validation:",test_index)
       X_train, X_test = X[train_index], X[test_index] 
       y_train, y_test = y[train_index], y[test_index]
-      X_train = sc.fit_transform(X_train)
+
+      #normalize the data (feature scaling)
+      X_train = sc.fit_transform(X_train) 
       X_test = sc.transform(X_test)
 
-      classifier.fit(X_train, y_train)
-      y_pred = classifier.predict(X_test)
-      score.append(classifier.score(X_test,y_test))
-      cmRF.append(confusion_matrix(y_test, y_pred))
-      y_test_1.append(y_test)      
-      y_pred_1.append(y_pred)
+      classifier.fit(X_train, y_train)       #train the model
+      y_pred = classifier.predict(X_test)	 #Test the model
+      score.append(classifier.score(X_test,y_test)) #save the accuracy
+      cmRF.append(confusion_matrix(y_test, y_pred)) #save the confusion matrix
+      y_test_1.append(y_test)      #save the prediction value
+      y_pred_1.append(y_pred)      #save the real value
       
       classifierNB.fit(X_train, y_train)
       y_pred_NB = classifier.predict(X_test)
